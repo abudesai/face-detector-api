53f37f50afafe13578d614667b9129e46d0740c1
Abu Desai <abhyuday.desai@gmail.com>
2018-04-29 10:46:27 -0700

    database connection updated to heroku postgres

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'test.user',
-    password : 'testpassword',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
@@ -64,12 +62,9 @@ app.get('/', (req, res) => {
 	res.send('this is working.');
 })
 
-app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
-
+app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)});
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)} );
-
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
-
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
