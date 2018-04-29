67d84146ed8147f2b0dab4d85f5be5a87a2d3e5b
Abu Desai <abhyuday.desai@gmail.com>
2018-04-29 10:58:10 -0700

    fix2

--- controllers/signin.js
+++ controllers/signin.js
@@ -3,6 +3,10 @@
 const handleSignIn =  (req, res, db, bcrypt) => { 
 
 	const {email, password} = req.body;	
+	if (!email || !password) {
+		return res.status(400).json('Invalid form submission.')
+	}
+
 	db.select('email', 'hash').from('login')
 		.where('email', '=', email)
 		.then(users => {
